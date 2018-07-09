728a667fbd8f6835e05cc2722af433a9c0baf74e
egbertchang <egbertchang@icloud.com>
2018-07-09 12:43:43 +0800

    fix: 去掉阿里云内外IP地址

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-  "123_hao_dai/util"
   "123_hao_dai/elea"
   "123_hao_dai/src/server"
 )
 
 func main() {
-  ip := util.LocalIP()
   Server := elea.Server{
-    Addr: ip + ":8080",
+    Addr: ":8080",
     Handle: server.Handle(),
     Interceptor: &server.HttpInterceptor{},
   }
